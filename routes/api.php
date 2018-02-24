f52e65a7a0e08eb51f60ea19855a934d2e453f36
Lucas Pearson <lpearson@five-startech.com>
2018-02-24 02:02:19 -0500

    Still just testing routes

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,26 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+$router->get('generateTickets', function() use($router){
+  factory(\App\Ticket::class, 10)->create()->each(function($ticket){
+    $notes = factory(\App\Note::class, rand(1,5))->make();
+    $notes->each(function($note) use ($ticket){
+      $ticket->notes()->save($note);
+    });
+    $owners = factory(\App\User::class, rand(1,5))->make();
+    $creator = factory(\App\User::class)->create();
+    $ticket->owners()->saveMany($owners);
+    $ticket->creator()->save($creator);
+    $ticket->save();
+  });
+});
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+$router->get('/tickets', function () use ($router) {
+    $tickets = \App\Ticket::with(['notes', 'owners', 'creator'])->get();
+    return $tickets;
+});
+
+$router->get('/users', function () use ($router) {
+    $users = \App\User::all();
+    return $users;
 });
