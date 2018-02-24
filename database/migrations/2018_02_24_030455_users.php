3995e0ab2704a921a74a2a8d78748494f1f39882
Lucas Pearson <lpearson@five-startech.com>
2018-02-23 23:26:23 -0500

    User migration

---
+++ database/migrations/2018_02_24_030455_users.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Users extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('users', function($collection){
+        $collection->index('name');
+        $collection->unique('email');
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
